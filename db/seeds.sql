ec6b4e3e825c7788165ff3aff61e1600728d0667
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-26 11:25:24 -0400

    Added burgers to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+(id,burger_name,devoured)
+VALUES (1,"veggie burger", TRUE);
+
+INSERT INTO burgers
+(id,burger_name,devoured)
+VALUES (2,"bison burger", TRUE);
+
+INSERT INTO burgers
+(id,burger_name,devoured)
+VALUES (3,"grass fed burger", TRUE)
+
+
