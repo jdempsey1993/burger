0089ce26522b83b649f9972aa3ea2bc076212caa
Jack Dempsey <jdempsey1993@gmail.com>
2019-12-03 16:26:59 -0500

    Fixed model file error

--- models/burger.js
+++ models/burger.js
@@ -17,8 +17,8 @@ var burger = {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
-  },
+  }
 };
 
 // Export the database functions for the controller.
-module.exports = burgers;
+module.exports = burger;
